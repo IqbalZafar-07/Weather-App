8857cbc13caec8fb33dc6fcf197f256132e11185
Iqbal <iqbalzafar909700@gmail.com>
2021-02-12 18:10:03 +0530

    second commit

--- src/components/CityInput/index.js
+++ src/components/CityInput/index.js
@@ -1,41 +1,38 @@
-import React from 'react';
-import {Form} from "react-bootstrap";
-import {Button} from "react-bootstrap";
+import React from "react";
+import { Form, Button } from "react-bootstrap";
 import "./style.css";
-const CityInput = ({city,setCity,fetchCityWeather}) => {
-    
-    const [error,setError] = React.useState("");
-   
-    const handleInputChange =(e)=>{
-        setCity(e.target.value);
-    };
-
-    const handleSubmit = () =>{
-        if(!city) {
-            setError("City field is empty");
-        }
-        else {
-            setError("");
-           
-            fetchCityWeather();
-        }
-    };
-    return (
-        <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>City</Form.Label>
-          <Form.Control 
-          value={city} type="text" placeholder="Enter city name"
-          onChange={handleInputChange} 
-          />
-          <p className="text-danger">{error}</p>
-        </Form.Group>
-
-        <Button variant="primary" onClick={handleSubmit}>
-          Submit
+const CityInput = ({ city, setCity, fetchCityWeather }) => {
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+  const [error, setError] = React.useState("");
+  const handleSubmit = () => {
+    if (!city) {
+      setError("City Field is empty!!");
+    } else {
+      fetchCityWeather();
+    }
+  };
+  return (
+    <Form className="form1">
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label className="cityname">Enter City Name :</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="City(eg : darbhanga)"
+          id="input"
+          value={city}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <p className="text-danger">{error}</p>
+      <div>
+        <Button className="button1" variant="info" onClick={handleSubmit}>
+          Check
         </Button>
-      </Form>
-    );
+      </div>
+    </Form>
+  );
 };
 
 export default CityInput;
